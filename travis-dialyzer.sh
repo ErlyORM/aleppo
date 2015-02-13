ec9aef4a81c8c971692a7a243b490b9bd6ff5031
danikp <dmitry.polyanovsky@gmail.com>
2015-02-13 11:03:35 +0200

    cleaner plt

--- travis-dialyzer.sh
+++ travis-dialyzer.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-
 PLT=plt/aleppo-$RANDOM.plt
 echo "PLT File $PLT"
 export PATH=$PATH:/usr/local/bin:/usr/bin
 echo "Building PLT, may take a few minutes"
 dialyzer  --build_plt --apps kernel stdlib\
        --output_plt $PLT > /dev/null
-for app in  mnesia  inets ssl crypto \
-       erts public_key runtime_tools compiler asn1 hipe\
-       syntax_tools 
+
+echo "********************************************************************************"
+for app in  compiler 
 do 
     echo $app
     dialyzer --add_to_plt --apps $app\
        --plt $PLT > /dev/null
 done
-rm -f deps/riak_*/ebin/*_pb.beam  
+rm -f deps/riak_*/ebin/*_pb.beam
+
 echo "********************************************************************************"
 for app in $(ls deps/)
 do
    echo "Adding $app"
    dialyzer --add_to_plt --apps deps/$app \
        --plt $PLT > /dev/null
-
-
 done
 echo "********************************************************************************"
 echo ""
@@ -35,8 +33,5 @@ dialyzer	ebin/		\
     --statistics -n             \
     --fullpath			\
     -n                          \
-    --plt $PLT #  -Wunmatched_returns 
-#  
-
-
-
+    --plt $PLT #  -Wunmatched_returns
+#
