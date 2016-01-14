14b1ff148b5141d9962ac39da4db615c48797d5e
Brujo Benavides <elbrujohalcon@inaka.net>
2016-01-13 23:18:55 -0300

    [#18] Fix @elvisci warnings

--- src/aleppo.erl
+++ src/aleppo.erl
@@ -1,6 +1,7 @@
 % Aleppo: ALternative Erlang Pre-ProcessOr
 -module(aleppo).
--export([process_file/1, process_file/2, process_tokens/1, process_tokens/2, scan_file/1]).
+-export([process_file/1, process_file/2, process_tokens/1, process_tokens/2,
+         scan_file/1]).
 
 -record(ale_context, {
         include_trail = [],
@@ -15,7 +16,8 @@ process_file(FileName, Options) ->
     ModuleName = list_to_atom(filename:rootname(filename:basename(FileName))),
     case scan_file(FileName) of
         {ok, Tokens} ->
-            process_tokens(Tokens, [{file, FileName}, {module, ModuleName}|Options]);
+            process_tokens(
+                Tokens, [{file, FileName}, {module, ModuleName}|Options]);
         Error ->
             Error
     end.
@@ -451,11 +453,15 @@ reverse_and_normalize_token_locations(RevTokens) ->
 
 reverse_and_normalize_token_locations_helper([], Acc) ->
     Acc;
-reverse_and_normalize_token_locations_helper([{Type, MaybeLocation} | Rest], Acc) when is_tuple(MaybeLocation) orelse
-                                                                                       is_list(MaybeLocation) ->
-    reverse_and_normalize_token_locations_helper(Rest, [{Type, location(MaybeLocation)}|Acc]);
-reverse_and_normalize_token_locations_helper([{Type, MaybeLocation, Extra} | Rest], Acc) when is_tuple(MaybeLocation) orelse
-                                                                                              is_list(MaybeLocation) ->
-    reverse_and_normalize_token_locations_helper(Rest, [{Type, location(MaybeLocation), Extra}|Acc]);
+reverse_and_normalize_token_locations_helper(
+    [{Type, MaybeLocation} | Rest], Acc)
+        when is_tuple(MaybeLocation) orelse is_list(MaybeLocation) ->
+    reverse_and_normalize_token_locations_helper(
+        Rest, [{Type, location(MaybeLocation)}|Acc]);
+reverse_and_normalize_token_locations_helper(
+    [{Type, MaybeLocation, Extra} | Rest], Acc)
+        when is_tuple(MaybeLocation) orelse is_list(MaybeLocation) ->
+    reverse_and_normalize_token_locations_helper(
+        Rest, [{Type, location(MaybeLocation), Extra}|Acc]);
 reverse_and_normalize_token_locations_helper([Other | Rest], Acc) ->
     reverse_and_normalize_token_locations_helper(Rest, [Other | Acc]).
